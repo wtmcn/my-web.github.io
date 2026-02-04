d913751af077cb37977aab86ff64f4d8565935ee
wtmcn <3094982002@qq.com>
2026-02-04 18:03:52 +0800

    Update 登录.js

--- 登录.js
+++ 登录.js
@@ -5,10 +5,11 @@ loginBtn.addEventListener('click',function(){
 	var password = document.getElementById('password').value;
 	if(username === '奔赴' || password === '123456'){
 		alert("登陆成功……即将跳转")
-		window.location.href = 'https://www.1024txt.com/book/600105/'
+		window.location.href = 'https://wtmcn.github.io/'
 		
 	}else{
 	alert('用户名或密码错误，请重试！')
 	return;
 		}
 })
+
